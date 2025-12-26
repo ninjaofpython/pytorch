8f3520fb6f18ec956650a5528f6c417fbb7bbf08
mengeworkman <mengeworkman@gmail.com>
2025-12-25 23:13:34 -0800

    Reshape and slice

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ my_list =  [[1, 2, 3, 4], [6, 7, 8, 9]]
 print(my_list)
 
 np1 = np.random.rand(3, 4)
+print("This is my numpy array xxxxxx")
 print(np1)
 print(np1.dtype)
 
 
 tensor_2d = torch.randn(3, 4)
+print("Here's my 2d tensor xxxxxxx")
 print(tensor_2d)
+print("Here's my tensor 2d type")
+print(tensor_2d.dtype)
+
+tensor_3d = torch.zeros(2, 3, 4)
+print(tensor_3d)
+
+## Create tensor out of numpy array
+my_tensor = torch.tensor(np1)
+print("Hers's my_tensor xxxxxxx")
+print(my_tensor)
