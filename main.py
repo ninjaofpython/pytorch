ed0a5219bb0bae372ddd88ba80e9129239c2b693
mengeworkman <mengeworkman@gmail.com>
2025-12-25 20:53:45 -0800

    Introducting tensors

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from pytorch!")
+import numpy as np
+import torch
+my_list =  [[1, 2, 3, 4], [6, 7, 8, 9]]
+print(my_list)
+
+np1 = np.random.rand(3, 4)
+print(np1)
+print(np1.dtype)
 
 
-if __name__ == "__main__":
-    main()
+tensor_2d = torch.randn(3, 4)
+print(tensor_2d)
