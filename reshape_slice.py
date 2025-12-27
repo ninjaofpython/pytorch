f4d8fc104e13eb0cd1c624baa3dd8d7c117edbbe
mengeworkman <mengeworkman@gmail.com>
2025-12-27 00:04:04 -0800

    Slicing

--- reshape_slice.py
+++ reshape_slice.py
@@ -16,6 +16,33 @@ print(my_torch2)
 # If you don't know the number of items in the tensor then use -1 to reshape
 # But the division of numbers need to make sense a tensor with 15 items won't work
 # due to it's not divisible by (2, -1) but (3, -1) will due to 15 is divisble by 3
-my_torch2 = my_torch2.reshape(2, -1)
+
+my_torch2 = my_torch2.reshape(3, -1)
 print("Here's my_torch I'm not sure how may reshaping")
 print(my_torch2)
+
+# You can also do the same in the opposite direction
+my_torch2 = my_torch2.reshape(-1, 3)
+print("Here's my_torch2 in the opposite direction xxxxxx")
+print(my_torch2)
+
+# Here's a lesson on views
+my_torch3 = torch.arange(10)
+my_torch4 = my_torch3.view(2, 5)
+print("Here's my my_torch4 with a view xxxxxx")
+print(my_torch4)
+
+# Differences between shape and view
+# with reshape and view, they will update
+my_torch5 = torch.arange(10)
+my_torch6 = my_torch5.reshape(2, 5)
+print("Here's my_torch6 which is my reshaped my_torch5 xxxxxx")
+print(my_torch6)
+my_torch5[1] = 4141
+print("Here's my new entry to the original tensor my_torch5 with 4141")
+print(my_torch5)
+print("Here's my new my_torch6 xxxxxxx")
+print(my_torch6)
+
+
+
