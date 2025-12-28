5337f44867291f64ecbfdf0f8a3d725b0575f305
mengeworkman <mengeworkman@gmail.com>
2025-12-27 21:36:20 -0800

    Subtraction

--- tensor_math.py
+++ tensor_math.py
@@ -1,4 +1,23 @@
 import torch
 import numpy as np
 
+# Basic tensor math operations
+
+tensor_a = torch.tensor([1, 2, 3, 4])
+tensor_b = torch.tensor([5, 6, 7, 8])
+
+
+# Addition
+print("Adding tensor_a and tensor_b with actual arithmetic xxxxxxxxx.")
+print(tensor_a + tensor_b)
+
+
+# Addition Longhand result same as above
+print("Addition 'Longhand' but clean syntax xxxxxx.")
+print(torch.add(tensor_a, tensor_b))
+
+
+# Subtraction
+print("Here's subtraction with Pytorch xxxxxx.")
+print(tensor_b - tensor_a)
 
